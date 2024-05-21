ecbea83c4d4020b91de5fc5d3df70cad24b97abf
Andrew Molten <andrewmolten23@gmail.com>
2024-05-21 21:08:14 +1200

    get corrections & new words to display

--- client/components/StoryDifference.tsx
+++ client/components/StoryDifference.tsx
@@ -9,6 +9,16 @@ interface Message {
   message: { content: string; role: string }
 }
 
+interface Correction {
+  original: string
+  correction: string
+}
+
+interface NewWord {
+  word: string
+  meaning: string
+}
+
 // use array.map tp map over corrections and words to add to vocab, so that we can get the objects from it
 
 function StoryDifference({ data }: Props) {
@@ -20,6 +30,26 @@ function StoryDifference({ data }: Props) {
     <div>
       <h1>Differences</h1>
       <p>Proper translation: {messageContent.translatedGermanStory}</p>
+      <h2>Corrections</h2>
+      <ul>
+        {messageContent.corrections.map((correction: Correction) => {
+          return (
+            <li key={correction.original}>
+              {correction.original}: <strong>{correction.correction}</strong>
+            </li>
+          )
+        })}
+      </ul>
+      <h2>New words</h2>
+      <ul>
+        {messageContent.wordsToAddToVocabulary.map((newWord: NewWord) => {
+          return (
+            <li key={newWord.word}>
+              {newWord.word}: <strong>{newWord.meaning}</strong>
+            </li>
+          )
+        })}
+      </ul>
       {/* <p>Corrections: {messageContent.corrections}</p> */}
       {/* <p>New words: {messageContent.wordsToAddToVocabulary}</p> */}
     </div>
