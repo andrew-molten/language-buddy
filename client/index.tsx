2983cffdd365fc1632fb13426b039fab0b4aca34
Andrew Molten <andrewmolten23@gmail.com>
2024-06-17 20:22:30 +1200

    install auth0

--- client/index.tsx
+++ client/index.tsx
@@ -4,14 +4,27 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 
 import router from './router.tsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>,
+    <Auth0Provider
+      domain="whai-2024-andrew.au.auth0.com"
+      clientId="uP5YLX5lLn6aTi251qbXTu8C50nlrYtB"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Auth0Provider>,
   )
 })
+
+// ClientId: uP5YLX5lLn6aTi251qbXTu8C50nlrYtB
+// domain: whai-2024-andrew.au.auth0.com
+// audience: https://language-buddy/api
