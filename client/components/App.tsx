68b9f498acb00a716c10c19c7a9f36a457e12142
Andrew Molten <andrewmolten23@gmail.com>
2024-07-17 17:04:41 +1200

    install tailwind

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from './NavBar'
 import { useGetUser } from '../hooks/useUser'
+import Registration from './Registration'
+import { useAuth0 } from '@auth0/auth0-react'
 
 // if a user exists with this Auth Id then set the user id
 
@@ -9,9 +11,30 @@ import { useGetUser } from '../hooks/useUser'
 // Otherwise display registration
 
 function App() {
-  // check user exists
+  const { isAuthenticated } = useAuth0()
   const user = useGetUser()
-  console.log(user.data)
+  if (isAuthenticated && user.isPending) {
+    return (
+      <div className="app">
+        <header className="header">
+          <h1>Language buddy </h1>
+        </header>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
+  if (isAuthenticated && !user.data.id)
+    // for new user
+    return (
+      <div className="app">
+        <header className="header">
+          <h1>Language buddy </h1>
+        </header>
+        <Registration />
+      </div>
+    )
+
   return (
     <>
       <div className="app">
--- client/components/Registration.tsx
+++ client/components/Registration.tsx
@@ -1,7 +1,45 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useState } from 'react'
+
 function Registration() {
+  const { user } = useAuth0()
+  const [formState, setFormState] = useState({
+    username: user?.nickname ? user.nickname : '',
+    birthdate: '',
+  })
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFormState({ ...formState, [event.target.name]: event.target.value })
+    console.log(event.target.value)
+  }
+
+  console.log(user)
   return (
-    <div>
-      <p>Sign in first</p>
+    <div className=" page">
+      <h2 className="page-heading">Registration</h2>
+      <form>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={formState.username}
+          onChange={handleChange}
+        ></input>{' '}
+        <br />
+        <span className="mt-3">
+          <label className="text-sky-400/100" htmlFor="birthdate">
+            Birthdate:{' '}
+          </label>
+          <input
+            name="birthdate"
+            id="birthdate"
+            type="date"
+            value={formState.birthdate}
+            onChange={handleChange}
+          />
+        </span>
+      </form>
     </div>
   )
 }
