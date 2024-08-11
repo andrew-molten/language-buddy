8df3e68ecd5840830b0e4f89b54bd39e247d1f68
Andrew Molten <74161919+andrew-molten@users.noreply.github.com>
2024-08-12 09:18:40 +1200

    giv guess-div min-height, change go-btn colour

--- client/components/phraseLessons/WordChunks.tsx
+++ client/components/phraseLessons/WordChunks.tsx
@@ -163,7 +163,7 @@ function WordChunks({ phrase, setProgress, progress, handleFinish }: Props) {
   return (
     <div>
       <p>{phrase.translation}</p>
-      <div>
+      <div className="guess-div">
         {guessSentence.map((word, i) => (
           <button
             className="word-btn"
